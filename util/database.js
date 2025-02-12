d3c9f14402ff1c7c74f4a6ffc89fd1481b80be08
ZoranSpasenovic <spalespasenovic@gmail.com>
2025-02-12 09:22:45 +0100

    ip bug

--- util/database.js
+++ util/database.js
@@ -1,14 +1,11 @@
 const Sequelize = require("sequelize");
 
-const databaseUrl =
-  process.env.DATABASE_URL ||
-  "mysql://root:thuglife7@localhost:3306/tropical_glow";
+const databaseUrl = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(databaseUrl, {
   dialect: "mysql",
   logging: false,
 });
-
 
 sequelize
   .authenticate()
